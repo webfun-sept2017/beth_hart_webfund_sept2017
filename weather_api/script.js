5e401b28ebd54a241175cd3acb3c7eac6389992e
Beth Hart <bethhart27@gmail.com>
2017-10-03 16:24:06 +0000

    add html template to js

--- weather_api/script.js
+++ weather_api/script.js
@@ -1,11 +1,15 @@
 
 $(document).ready(function(){
-	$("form").on(click, "#submit", function(e) {
+	$("form").on("click", "#submit", function(e) {
 		e.preventDefault();
 		var city = $("#city").val()
-		$.get("api.openweathermap.org/data/2.5/weather?q=${city}&APPID=8f9c0e3c39482fd48101fa130b0129", function success(res){
+		console.log(city);
+		$.get("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=8f9c0e3c39482fd48101fa130b012981", function success(res){
 			console.log(res);
-			$("#weather").html(`<h2>${forecast}</h2>`);
+			$("#weather").html(`<h2>Currently in ${city}:</h2>
+			<p>humidity: ${res.main.humidity} </p>
+			<p>high: ${res.main.temp_max}</p>
+			<p>low: ${res.main.temp_min}</p>`)
 
 	}, "json");
 	
